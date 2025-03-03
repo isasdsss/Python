a7516bd1945ec364ddfb408889d734f2f6e1f5e4
Isabela Sampaio <148920193+isasdsss@users.noreply.github.com>
2025-03-03 16:41:11 -0300

    Comentários
    explicação das funções

--- Calculadora/main.py
+++ Calculadora/main.py
@@ -30,40 +30,44 @@ frame_corpo.grid(row=1, column=0)
 
 #variável todos os valores
 
-todos_valores = ""
+todos_valores = ""  # inicia uma string vazia para armazenar os valores digitados pelo usuário ao longo do uso da calculadora.
 
-# Criando funçao
+#funçao : Essa função é responsável por armazenar e exibir os valores digitados conforme o usuário interage com a interface.
 def entrar_valores(event):
     
-    global todos_valores
+    global todos_valores #será modificada dentro da função. Isso permite que seu valor seja atualizado e mantido a cada entrada do usuário.
 
-    todos_valores = todos_valores + str(event)
+    todos_valores += str(event)
 
     #passando o valor para a tela
     valor_texto.set(todos_valores)
 
 
-    #Funcção para calcular
-
+#Função para calcular
 def calcular():
-    global todos_valores
-    resultado= eval(todos_valores)
-    print(resultado)
+    global todos_valores     # A variável 'todos_valores' é global e acessada dentro da função
+    resultado= eval(todos_valores)   # A função 'eval()' avalia o conteúdo de 'todos_valores' como uma expressão Python e armazena o resultado
+    print(resultado)    
 
-    valor_texto.set(str(resultado))
+    valor_texto.set(str(resultado))        # Atualiza a variável 'valor_texto' com o resultado calculado, transformando-o em string
+    # A mudança no 'valor_texto' faz o 'Label' que está associado a ele ser atualizado automaticamente
+
 
     #Função limpar tela
 def limpar_tela():
-    global todos_valores
-    todos_valores = ""
-    valor_texto.set("")
+    global todos_valores  # Acessa a variável global 'todos_valores'
+    todos_valores = ""    # Limpa os valores armazenados para o próximo cálculo
+    valor_texto.set("")   # Limpa o conteúdo exibido na tela
         
 
 
 #LABEL
 
+# Criação de um objeto 'StringVar' que irá armazenar o valor do texto para o 'Label'
 valor_texto = StringVar()
 
+
+# Criação de um 'Label' com a variável 'valor_texto' como conteúdo de texto
 app_label = Label(frame_tela, textvariable=valor_texto, width=15, height=2, padx=7, relief=FLAT, anchor="e", justify="right", font=('Ivy 18 bold'), bg=cor1, fg=cor2)
 app_label.place(x=0, y=0)
 
