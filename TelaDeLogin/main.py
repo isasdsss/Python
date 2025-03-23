7adfef0e8856c1a925e4b225c52c4ccc080459eb
Isabela Sampaio <148920193+isasdsss@users.noreply.github.com>
2025-03-23 11:23:38 -0300

    Login
    Implementando login do usuário

--- TelaDeLogin/main.py
+++ TelaDeLogin/main.py
@@ -16,6 +16,46 @@ frame_cima.grid(row=0, column=0, pady=1, padx=0, sticky=NSEW )
 frame_baixo = Frame(janela, width=310, height=250, bg='white', relief='flat')
 frame_baixo.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW )
 
+
+#Funções
+
+credencial = ['Isabela', '123456789']
+# funçao verificar senha
+def verificar_senha():
+    nome = e_nome.get()
+    senha = e_senha.get()
+
+    if nome == 'admin' and senha =='admin':
+        messagebox.showinfo("Login", "Login realizado com sucesso!")
+    elif credencial[0] == nome and credencial[1] == senha:
+        messagebox.showinfo("Login", "Login realizado com sucesso!")
+
+        #apaga o frame baixo e cima
+        for widget in frame_baixo.winfo_children():
+            widget.destroy()
+
+        for widget in frame_cima.winfo_children():
+            widget.destroy()
+
+        nova_janela()
+    else:
+        
+        messagebox.showerror("Erro", "Nome ou senha incorretos!")
+
+#funçao apos vereficar
+def nova_janela():
+
+    l_login = Label(frame_cima, text='Usuário: ' + credencial[0], anchor=NE, font=('Ivy 12'), fg='black', bg='white')
+    l_login.place(x=5, y=5)
+
+    l_linha = Label(frame_cima, width=275, anchor=NW, font=('Ivy 1 bold'), bg='purple')
+    l_linha.place(x=10, y=46)
+
+    l_login = Label(frame_baixo, text='Seja Bem-vindo(a): ' + credencial[0], anchor=NE, font=('Ivy 12'), fg='black', bg='white')
+    l_login.place(x=5, y=5)
+
+
+
 #Configurando Frame_Cima (label)
 
 l_login = Label(frame_cima, text='LOGIN', anchor=NE, font=('Ivy 25'), fg='black', bg='white')
@@ -44,7 +84,7 @@ e_senha = Entry(frame_baixo, width=25, justify='left', show='*', font=('', 15), 
 e_senha.place(x=14, y=125)
 
 #Botão
-b_confirmar = Button(frame_baixo,text='Enviar', width=40, height=2, bg='purple', fg='white', font='Ivy 8 bold', relief='raised', overrelief='ridge')
+b_confirmar = Button(frame_baixo,command=verificar_senha, text='Enviar', width=40, height=2, bg='purple', fg='white', font='Ivy 8 bold', relief='raised', overrelief='ridge')
 b_confirmar.place(x=10, y=180)
 
 janela.mainloop()
